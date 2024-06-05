1ba8423daaded099f1f511cef626a0b1bc26c508
seno0010 <mayamaya521999@gmail.com>
2024-06-06 04:03:01 +0900

    作業コミット

--- Assets/Myapp/Script/ModelScript.cs
+++ Assets/Myapp/Script/ModelScript.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 public class ModelScript : MonoBehaviour
 {
 
-    private GameObject _emptyObject; // 空のオブジェクトを参照
-    private float _maxDistance = 0f; // モデルが空オブジェクトから離れる最大距離
-    private float _speed = 5f; // モデルが空オブジェクトに向かう速度
+    private GameObject _emptyObject; // カメラの先にある目標となるオブジェクトを参照
+    private float _maxDistance = 0.1f; // モデルが目標オブジェクトから離れる最大距離
+    private float _speed = 5f; // モデルが目標オブジェクトに向かう速度
     private InstiatePrehab instiatePrehab;
 
-    private Camera _mainCamera;
+    private Camera _mainCamera;　//メインカメラの参照
+    private Animator _animator;　//モデルが持つアニメーターコンポーネント用
     void Start()
     {
         GameObject gmb = GameObject.Find("Launcher");
@@ -18,13 +19,15 @@ public class ModelScript : MonoBehaviour
         //↑モデル移動関数で使用
 
         _mainCamera = Camera.main;
+        _animator = GetComponent<Animator>();
     }
 
 
     void Update()
     {
         MoveModel();
-      //  ModelLookatCamera();
+       // ModelLookatCamera();
+        //UpdateAnimatorParameters();
     }
 
 
@@ -32,7 +35,7 @@ public class ModelScript : MonoBehaviour
 
 
     //モデル移動関数
-    //カメラ本体の移動があればモデルも追従するがカメラ回転には合わせて移動しない
+    //移動する方向に向かって回転までする
     private void MoveModel()
     {
         if (instiatePrehab != null && _emptyObject != null)
@@ -47,7 +50,6 @@ public class ModelScript : MonoBehaviour
                 // モデルを空オブジェクトの最新の位置に向かってX軸とZ軸方向に移動
                 Vector3 targetPosition = new(_emptyObject.transform.position.x, transform.position.y, _emptyObject.transform.position.z);
                 Vector3 direction = targetPosition - transform.position;
-
                 // 回転を設定
                 if (direction != Vector3.zero)
                 {
@@ -58,11 +60,13 @@ public class ModelScript : MonoBehaviour
                 // 移動を設定
                 float step = _speed * Time.deltaTime;
                 transform.position = Vector3.Slerp(transform.position, targetPosition, step);
+                
             }
         }
     }
 
 
+    //スマホカメラの方向を常に見続ける
     private void ModelLookatCamera()
     {
         if (_mainCamera != null)
@@ -71,5 +75,25 @@ public class ModelScript : MonoBehaviour
             direction.y = 0; // Y軸の回転を無視する場合はコメントを外す
             transform.rotation = Quaternion.LookRotation(direction);
         }
-    } 
+    }
+
+
+    //X方向への差が多き場合に移動するためその時にアニメーションの歩きを再生するよう遷移させる
+    private void UpdateAnimatorParameters()
+    {
+        if (_animator != null)
+        {
+            float distanceX = Mathf.Abs(transform.position.x - _emptyObject.transform.position.x);
+            // 例えば、モデルが移動しているかどうかを判定してアニメーターに伝える
+            if (distanceX > _maxDistance)
+            {
+                _animator.SetBool("isMoving", true);
+            }
+            else
+            {
+                _animator.SetBool("isMoving", false);
+            }              
+             
+        }
+    }
 }
