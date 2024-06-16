074c8e11a4fe16a806ba4b174c5aa827541de30d
seno0010 <mayamaya521999@gmail.com>
2024-06-17 01:23:23 +0900

    一旦全部ではあるが画面内の認識できるものをテキストで表示できるようになった

---
+++ Assets/Myapp/Script/Objectdetection.cs
@@ -0,0 +1,22 @@
+using Niantic.Lightship.AR.ObjectDetection;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objectdetection : MonoBehaviour
+{
+    // オブジェクト検出マネージャーを管理するためのシリアライズフィールド
+    [SerializeField]
+    private ARObjectDetectionManager _objectDetectionManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
