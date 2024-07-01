7c7d22689d74cc0aa72761443c0c0b2154c70fbd
seno0010 <mayamaya521999@gmail.com>
2024-07-01 21:03:23 +0900

    別モデルの追加と参考モーションの追加

--- Assets/Myapp/Script/Launcher.cs
+++ Assets/Myapp/Script/Launcher.cs
@@ -16,26 +16,20 @@ public class Launcher : MonoBehaviour
         if (Input.touchCount > 0)
         {
             Touch touch = Input.touches[0];
-            if (touch.phase == TouchPhase.Began)
-            {
-
                 if (touch.phase == TouchPhase.Began)
-
                 {
-
                     var _ray = Camera.main.ScreenPointToRay(touch.position);
                     if (Physics.Raycast(_ray, out _hit))
 
                     {
-
                         Instantiate(_modelprefab, _hit.transform.position, _hit.transform.rotation);
                         GameObject emptyobj = Instantiate(_emptyobjprehab, _hit.transform.position, _hit.transform.rotation);
                         emptyobj.transform.SetParent(Camera.main.transform);
                         ModelScript modelscp= _modelprefab.GetComponent<ModelScript>();
                        // modelscp._emptyObject = emptyobj;
                     }
-                }
-            }
+                } 
+            
 
         }
     }
